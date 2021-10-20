e7fea59a3cfa10f83e5761d2ab20f61da0425a2d
Tanseer Saji <tanseersaji@gmail.com>
2021-10-20 12:55:49 +0400

    added skeleton class for MuseDevice

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 #include <blepp/lescan.h>
-#include <boost/optional/optional_io.hpp>
+#include <blepp/blestatemachine.h>
+
 
 using namespace std;
 using namespace BLEPP;
 
-int main() {
-    
-//    log_level = LogLevels::Info;
-    HCIScanner scanner;
+class MuseDevice{
+    public:
+        string name;
+        string address;
+        int rssi;
 
-    while (1) {
+        bool connect() {
+            cout << "Attempting to connect to " << this->name << " with address " << this->address << endl;
+        }
+
+};
+
+int main() {
+    HCIScanner scanner;
+    BLEGATTStateMachine gatt;
+    MuseDevice device;
+
+    bool flag = true;
+
+    while (flag) {
             vector<AdvertisingResponse> ads = scanner.get_advertisements();
             for (AdvertisingResponse ad : ads) {
                 if (ad.local_name.is_initialized()) {
@@ -18,11 +33,23 @@ int main() {
                         cout << "Found: " << ad.local_name->name << endl;
                         cout << "Address: " << ad.address << endl;
                         cout << "RSSI: " << int(ad.rssi) << endl;
+
+                        device.name = ad.local_name->name;
+                        device.address = ad.address;
+                        device.rssi = int(ad.rssi);
+                        flag = false;
                         break;
                     }
                 }
             }
     }
-    
+
+    if (device.connect()) {
+        cout << "Successfully Connected" << endl;
+    } else {
+        cout << "Could not connect to " << device.name << endl;
+        return 1;
+    }
+
     return 0;
 }
