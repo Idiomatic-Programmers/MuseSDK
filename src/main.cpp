337c81bc345e71228dcc0b6490c057d9d98fa37e
Tanseer Saji <tanseersaji@gmail.com>
2021-10-20 12:27:17 +0400

    Scan and find MuseS device

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <blepp/lescan.h>
+#include <boost/optional/optional_io.hpp>
 
 using namespace std;
 using namespace BLEPP;
 
 int main() {
     
-    log_level = LogLevels::Info;
+//    log_level = LogLevels::Info;
     HCIScanner scanner;
 
     while (1) {
-        std::vector<AdvertisingResponse> ads = scanner.get_advertisements();
-    }    
+            vector<AdvertisingResponse> ads = scanner.get_advertisements();
+            for (AdvertisingResponse ad : ads) {
+                if (ad.local_name.is_initialized()) {
+                    if (ad.local_name->name.find("MuseS") != string::npos) {
+                        cout << "Found: " << ad.local_name->name << endl;
+                        cout << "Address: " << ad.address << endl;
+                        cout << "RSSI: " << int(ad.rssi) << endl;
+                        break;
+                    }
+                }
+            }
+    }
     
     return 0;
 }
