6c66c00e801935596dc800de8be8a0c24699eefc
Tanseer Saji <tanseersaji@gmail.com>
2021-10-20 10:51:33 +0400

    fixed linking error

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <blepp/lescan.h>
+
+using namespace std;
+using namespace BLEPP;
 
 int main() {
-    std::cout << "Muse SDK \n";
+    
+    log_level = LogLevels::Info;
+    HCIScanner scanner;
+
+    while (1) {
+        std::vector<AdvertisingResponse> ads = scanner.get_advertisements();
+    }    
+    
     return 0;
 }
